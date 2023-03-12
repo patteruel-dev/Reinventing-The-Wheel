a5c7ac7f795770e1639031256173ff5f17aa2ee0
John Patrick Teruel <jpteruel95@gmail.com>
2023-03-12 13:01:50 +0800

    Implemented floating point

--- Contents.swift
+++ Contents.swift
@@ -1,23 +1,45 @@
 import UIKit
 
+let negativeSignRegex = try! Regex(#"^-"#)
+
 struct PatInt: CustomStringConvertible {
     let isNegative: Bool
     let wholeNumberDigits: [Int8]
+    let decimalDigits: [Int8]
     let description: String
     let absolute: String
     init(_ raw: String) {
-        guard raw.contains(try! Regex(#"^(-)?\d+$"#)) else {
+        guard raw.contains(try! Regex(#"^(-)?\d+(.\d+)?$"#)) else {
             wholeNumberDigits = [0]
+            decimalDigits = [0]
             description = "0"
             absolute = description
             isNegative = false
             return
         }
-        isNegative = raw.contains(try! Regex(#"^-"#))
-        var trimmed = raw.trimmingPrefix(try! Regex(#"^-"#))
-        trimmed = trimmed.trimmingPrefix(try! Regex(#"^0+"#))
-        wholeNumberDigits = trimmed.compactMap({Int8(String($0))})
-        absolute = wholeNumberDigits.map{"\($0)"}.joined()
+        isNegative = raw.contains(negativeSignRegex)
+        var numberText = raw.trimmingPrefix(negativeSignRegex).trimmingPrefix(try! Regex(#"^0+"#))
+        if numberText.contains(try! Regex(#"\."#)) {
+            numberText = numberText.replacing(try! Regex(#"0+$"#), with: "")
+        } else {
+            numberText += ".0"
+        }
+        let numberComponents = numberText.split(separator: ".")
+        let wholeNumbersText = numberComponents[0] // this should be sure
+        let decimalText = numberComponents[1]
+        
+        wholeNumberDigits = wholeNumbersText.compactMap({Int8(String($0))})
+        decimalDigits = decimalText.compactMap({Int8(String($0))})
+        var absolute = wholeNumberDigits.map{"\($0)"}.joined()
+        
+        for digit in decimalDigits.reversed() {
+            if(digit == 0) {
+                continue
+            }
+            absolute += "." + decimalDigits.map{"\($0)"}.joined()
+            break
+        }
+        self.absolute = absolute
         description = (isNegative ? "-" : "") + absolute
     }
     
@@ -31,27 +53,59 @@ struct PatInt: CustomStringConvertible {
         }
         var lWholeDigits = lhs.wholeNumberDigits
         var rWholeDigits = rhs.wholeNumberDigits
-        let digits = max(lWholeDigits.count, rWholeDigits.count) + 1
-        while lWholeDigits.count < digits {
+        let wholeDigits = max(lWholeDigits.count, rWholeDigits.count) + 1
+        while lWholeDigits.count < wholeDigits {
             lWholeDigits.insert(0, at: 0)
         }
-        while rWholeDigits.count < digits {
+        while rWholeDigits.count < wholeDigits {
             rWholeDigits.insert(0, at: 0)
         }
-        var idx = digits - 1
+        
+        var lDecimalDigits = lhs.decimalDigits
+        var rDecimalDigits = rhs.decimalDigits
+        let decimalDigits = max(lDecimalDigits.count, rDecimalDigits.count)
+        while lDecimalDigits.count < decimalDigits {
+            lDecimalDigits.append(0)
+        }
+        while rDecimalDigits.count < decimalDigits {
+            rDecimalDigits.append(0)
+        }
+        
+        var decimalIdx = decimalDigits - 1
         var carried: Int8 = 0
+        var newDecimalDigits: [Int8] = []
+        while decimalIdx >= 0 {
+            let lDigit = lDecimalDigits[decimalIdx]
+            let rDigit = rDecimalDigits[decimalIdx]
+            let res = lDigit + rDigit + carried
+            let lastDigit = res % 10
+            carried = res / 10
+            newDecimalDigits.insert(lastDigit, at: 0)
+            decimalIdx -= 1
+        }
+        
+        var wholeIdx = wholeDigits - 1
         var newWholeDigits: [Int8] = []
-        while idx >= 0 {
-            let lDigit = lWholeDigits[idx]
-            let rDigit = rWholeDigits[idx]
+        while wholeIdx >= 0 {
+            let lDigit = lWholeDigits[wholeIdx]
+            let rDigit = rWholeDigits[wholeIdx]
             let res = lDigit + rDigit + carried
             let lastDigit = res % 10
             carried = res / 10
             newWholeDigits.insert(lastDigit, at: 0)
-            idx -= 1
+            wholeIdx -= 1
         }
         let isNegative = lhs.isNegative || rhs.isNegative
-        let final = (isNegative ? "-" : "") + newWholeDigits.map({"\($0)"}).joined()
+        var final = (isNegative ? "-" : "") + newWholeDigits.map({"\($0)"}).joined()
+        
+        for digit in newDecimalDigits.reversed() {
+            if(digit == 0) {
+                continue
+            }
+            final += "." + newDecimalDigits.map{"\($0)"}.joined()
+            break
+        }
+        
         return PatInt(final)
     }
     
@@ -63,40 +117,80 @@ struct PatInt: CustomStringConvertible {
         }
         var lWholeDigits = lhs.wholeNumberDigits
         var rWholeDigits = rhs.wholeNumberDigits
-        print("Initial: \(lWholeDigits.map {"\($0)"}.joined()), \(rWholeDigits.map {"\($0)"}.joined())")
-        let digits = max(lWholeDigits.count, rWholeDigits.count) + 1
-        while lWholeDigits.count < digits {
+        
+        let wholeDigits = max(lWholeDigits.count, rWholeDigits.count) + 1
+        while lWholeDigits.count < wholeDigits {
             lWholeDigits.insert(0, at: 0)
         }
-        while rWholeDigits.count < digits {
+        while rWholeDigits.count < wholeDigits {
             rWholeDigits.insert(0, at: 0)
         }
+        
+        var lDecimalDigits = lhs.decimalDigits
+        var rDecimalDigits = rhs.decimalDigits
+        let decimalDigits = max(lDecimalDigits.count, rDecimalDigits.count)
+        while lDecimalDigits.count < decimalDigits {
+            lDecimalDigits.append(0)
+        }
+        while rDecimalDigits.count < decimalDigits {
+            rDecimalDigits.append(0)
+        }
+        
         var isNegative = lhs.isNegative
-        for idx in 0..<digits {
-            if(lWholeDigits[idx] == rWholeDigits[idx]) {
+        let allDigits = wholeDigits + decimalDigits
+        let allLDigits = lWholeDigits + lDecimalDigits
+        let allRDigits = rWholeDigits + rDecimalDigits
+        print(allLDigits)
+        print(allRDigits)
+        for idx in 0..<allDigits {
+            if(allLDigits[idx] == allRDigits[idx]) {
                 continue
             }
-            if(rWholeDigits[idx] > lWholeDigits[idx]) {
+            if(allRDigits[idx] > allLDigits[idx]) {
                 isNegative = !rhs.isNegative
                 // swap
                 var temp = lWholeDigits
                 lWholeDigits = rWholeDigits
                 rWholeDigits = temp
+                
+                temp = lDecimalDigits
+                lDecimalDigits = rDecimalDigits
+                rDecimalDigits = temp
             }
             break
         }
-        print("Evaluating: \(lWholeDigits.map {"\($0)"}.joined()), \(rWholeDigits.map {"\($0)"}.joined())")
-        var idx = digits - 1
+        
+        var decimalIdx = decimalDigits - 1
         var borrowed: Int8 = 0
-        var newWholeDigits: [Int8] = []
-        while idx >= 0 {
-            var lDigit = lWholeDigits[idx] - borrowed
+        var newDecimalDigits: [Int8] = []
+        while decimalIdx >= 0 {
+            var lDigit = lDecimalDigits[decimalIdx] - borrowed
             borrowed = 0
             if lDigit < 0 {
                 borrowed = 1
                 lDigit += 10
             }
-            let rDigit = rWholeDigits[idx]
+            let rDigit = rDecimalDigits[decimalIdx]
+            if(lDigit < rDigit) {
+                borrowed = 1
+                lDigit += 10
+            }
+            let res = lDigit - rDigit
+            let lastDigit = res % 10
+            newDecimalDigits.insert(lastDigit, at: 0)
+            decimalIdx -= 1
+        }
+        
+        var wholeIdx = wholeDigits - 1
+        var newWholeDigits: [Int8] = []
+        while wholeIdx >= 0 {
+            var lDigit = lWholeDigits[wholeIdx] - borrowed
+            borrowed = 0
+            if lDigit < 0 {
+                borrowed = 1
+                lDigit += 10
+            }
+            let rDigit = rWholeDigits[wholeIdx]
             if(lDigit < rDigit) {
                 borrowed = 1
                 lDigit += 10
@@ -104,66 +198,80 @@ struct PatInt: CustomStringConvertible {
             let res = lDigit - rDigit
             let lastDigit = res % 10
             newWholeDigits.insert(lastDigit, at: 0)
-            idx -= 1
+            wholeIdx -= 1
         }
-        let final = (isNegative ? "-" : "") + newWholeDigits.map({"\($0)"}).joined()
+        var final = (isNegative ? "-" : "") + newWholeDigits.map({"\($0)"}).joined()
+        
+        for digit in newDecimalDigits.reversed() {
+            if(digit == 0) {
+                continue
+            }
+            final += "." + newDecimalDigits.map{"\($0)"}.joined()
+            break
+        }
         return PatInt(final)
     }
 }
 
 //print("P: \(PatInt("-0125"))")
 
-let sum: PatInt = PatInt("-135") + PatInt("-6223")
-print("Result: \(sum)")
+//let sum: PatInt = PatInt("-135") + PatInt("-6223")
+//print("Result: \(sum)")
+
+//let sum: PatInt = PatInt("-135.44") + PatInt("-6223.753")
+//print("Result: \(sum)")
+//
+//let diff: PatInt = PatInt("6203.72") - PatInt("135.69")
+//print("Result: \(diff)")
 //
 //
 //let diff: PatInt = PatInt("6203") - PatInt("135")
 //print("Result: \(diff)")
-let diff: PatInt = PatInt("135") - PatInt("6203")
-print("Result: \(diff)")
+//let diff: PatInt = PatInt("135") - PatInt("6203")
+//print("Result: \(diff)")
 
-var success = true
-for _ in 0..<100 {
-    let l = Int.random(in: -100...2000)
-    let r = Int.random(in: -100...2000)
-    let s = l + r
-
-    print("+!++++++")
-    let pl = PatInt("\(l)")
-    let pr = PatInt("\(r)")
-    let ps: PatInt = pl + pr
-
-    let isEqual = "\(s)" == "\(ps)"
-    success = success && isEqual
-    print("\(s) == \(ps) ? \(isEqual)")
-    if !isEqual {
-        print("Debug:")
-        print("L = \(l)")
-        print("R = \(r)")
-        print("PL = \(pl)")
-        print("PR = \(pr)")
-    }
-    print("++++++!+")
-}
-for _ in 0..<100 {
-    let l = Int.random(in: -1000...2000)
-    let r = Int.random(in: -1000...2000)
-    let d = l - r
-    
-    print("-!------")
-    let pl = PatInt("\(l)")
-    let pr = PatInt("\(r)")
-    let pd: PatInt = pl - pr
-    let isEqual = "\(d)" == "\(pd)"
-    success = success && isEqual
-    print("\(d) == \(pd) ? \(isEqual)")
-    if !isEqual {
-        print("Debug:")
-        print("L = \(l)")
-        print("R = \(r)")
-        print("PL = \(pl)")
-        print("PR = \(pr)")
-    }
-    print("------!-")
-}
-print("All Succeeded? \(success)")
+//var success = true
+//for _ in 0..<100 {
+//    let l = Double.random(in: -100...2000)
+//    let r = Double.random(in: -100...2000)
+//    let s = l + r
+//
+//    print("+!++++++")
+//    let pl = PatInt("\(l)")
+//    let pr = PatInt("\(r)")
+//    let ps: PatInt = pl + pr
+//
+//    let isEqual = "\(s)" == "\(ps)"
+//    success = success && isEqual
+//    print("\(s) == \(ps) ? \(isEqual)")
+//    if !isEqual {
+//        print("Debug:")
+//        print("L = \(l)")
+//        print("R = \(r)")
+//        print("PL = \(pl)")
+//        print("PR = \(pr)")
+//    }
+//    print("++++++!+")
+//}
+//for _ in 0..<100 {
+//    let l = Double.random(in: -1000...2000)
+//    let r = Double.random(in: -1000...2000)
+//    let d = l - r
+//
+//    print("-!------")
+//    let pl = PatInt("\(l)")
+//    let pr = PatInt("\(r)")
+//    let pd: PatInt = pl - pr
+//    let isEqual = "\(d)" == "\(pd)"
+//    success = success && isEqual
+//    print("\(d) == \(pd) ? \(isEqual)")
+//    if !isEqual {
+//        print("Debug:")
+//        print("L = \(l)")
+//        print("R = \(r)")
+//        print("PL = \(pl)")
+//        print("PR = \(pr)")
+//    }
+//    print("------!-")
+//}
+//print("All Succeeded? \(success)")
